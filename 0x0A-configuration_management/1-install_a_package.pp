97a4f1cd4c53a7cb03b5ab125adaf2539351aa7d
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-22 22:33:20 +0100

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# INstall an specific version of flask "2.1.0"
+# Install an specific version of flask "2.1.0"
 
-packege {'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
