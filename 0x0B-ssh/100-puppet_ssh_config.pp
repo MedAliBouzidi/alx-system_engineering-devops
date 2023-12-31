00b963b35fda8d6a3514471434af955b14456e7e
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-31 15:14:28 +0100

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Set up client SSH configuration file to connect to a server
+# without typing a password.
+include stdlib
+
+file_line{'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line{'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdendityFile ~/.ssh/school',
+  replace => true,
+}
